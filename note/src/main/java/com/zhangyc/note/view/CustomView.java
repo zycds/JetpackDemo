d67ef387e942abaa9c5ebb0aca49514b938495a3
zhangyongchao <zyc_ds@126.com>
2019-12-19 14:04:42 +0800

    custom view, remote maven;

---
+++ note/src/main/java/com/zhangyc/note/view/CustomView.java
@@ -0,0 +1,71 @@
+package com.zhangyc.note.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import androidx.annotation.RequiresApi;
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int size = MeasureSpec.getSize(widthMeasureSpec);
+        int mode = MeasureSpec.getMode(widthMeasureSpec);
+        int width = getWidth();
+
+        switch (mode) {
+            case MeasureSpec.AT_MOST:
+                width = size;
+                break;
+            case MeasureSpec.EXACTLY:
+                width = Math.min(width, size);
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                break;
+        }
+
+        MeasureSpec.makeMeasureSpec(width, mode);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
